b6247729b808ebe727ef452a694c8d9b40fcdfd9
Vinay14Newbie <wjust1984@gmail.com>
2023-11-09 10:59:20 +0530

    Tailwind added

---
+++ Tailwind/Setup/dst/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // content: ["./*.html"],  /* by doing this you can use tailwind to all files inside the tailwind folder */
+  content: ["./index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
